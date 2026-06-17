334bcb1338d394770b02da88de1393262e1031ee
iamhardyha <hachanghyeon411@gmail.com>
2026-06-17 21:54:20 +0900

    chore: scaffold Astro project

---
+++ astro.config.mjs
@@ -0,0 +1,13 @@
+import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
+import pagefind from 'astro-pagefind';
+
+export default defineConfig({
+  site: 'https://iamhardyha.github.io',
+  output: 'static',
+  integrations: [mdx(), sitemap(), pagefind()],
+  markdown: {
+    shikiConfig: { theme: 'github-light', wrap: true },
+  },
+});
